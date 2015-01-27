fd3c9bff01a366f9c84ce9836a8120675eb76e5e
Yingfei Wang <wyf@umich.edu>
2015-01-26 23:38:01 -0500

    change database config to mysql

--- DRRR2/settings.py
+++ DRRR2/settings.py
@@ -58,10 +58,22 @@ WSGI_APPLICATION = 'DRRR2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+import yaml
+config = yaml.load(file('config.yaml', 'r'))
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': config['NAME'],
+        'USER': config['USER'],
+        'PASSWORD': config['PASSWORD'],
     }
 }
 
